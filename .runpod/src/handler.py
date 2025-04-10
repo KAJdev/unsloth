16cf905e1ff30c0d30dd4e05494aeb68f11bdc0d
zeke <40004347+KAJdev@users.noreply.github.com>
2025-04-09 16:49:40 -0800

    fix

--- .runpod/src/handler.py
+++ .runpod/src/handler.py
@@ -8,26 +8,6 @@ import train
 def handler(job):
     job_input = job["input"]
 
-    # turn job_input into a class with attributes
-    class JobInput:
-        def __init__(self, **kwargs):
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-        def __getattr__(self, item):
-            return None
-
-        def __setattr__(self, key, value):
-            if key not in self.__dict__:
-                self.__dict__[key] = value
-            else:
-                raise AttributeError(f"Cannot set attribute {key}")
-
-        def get(self, key, default=None):
-            return getattr(self, key, default)
-
-    job_input = JobInput(**job_input)
-
     try:
         train.run(job_input)
     except subprocess.CalledProcessError as e:
--- .runpod/src/train.py
+++ .runpod/src/train.py
@@ -1,7 +1,41 @@
+#!/usr/bin/env python3
+
+"""
+🦥 Starter Script for Fine-Tuning FastLanguageModel with Unsloth
+
+This script is designed as a starting point for fine-tuning your models using unsloth.
+It includes configurable options for model loading, PEFT parameters, training arguments, 
+and model saving/pushing functionalities.
+
+You will likely want to customize this script to suit your specific use case 
+and requirements.
+
+Here are a few suggestions for customization:
+    - Modify the dataset loading and preprocessing steps to match your data.
+    - Customize the model saving and pushing configurations.
+
+Usage: (most of the options have valid default values this is an extended example for demonstration purposes)
+    python unsloth-cli.py --model_name "unsloth/llama-3-8b" --max_seq_length 8192 --dtype None --load_in_4bit \
+    --r 64 --lora_alpha 32 --lora_dropout 0.1 --bias "none" --use_gradient_checkpointing "unsloth" \
+    --random_state 3407 --use_rslora --per_device_train_batch_size 4 --gradient_accumulation_steps 8 \
+    --warmup_steps 5 --max_steps 400 --learning_rate 2e-6 --logging_steps 1 --optim "adamw_8bit" \
+    --weight_decay 0.005 --lr_scheduler_type "linear" --seed 3407 --output_dir "outputs" \
+    --report_to "tensorboard" --save_model --save_path "model" --quantization_method "f16" \
+    --push_model --hub_path "hf/model" --hub_token "your_hf_token"
+
+To see a full list of configurable options, use:
+    python unsloth-cli.py --help
+
+Happy fine-tuning!
+"""
+
+import argparse
 import os
 
 
 def run(args):
+    args = parse(args)
+
     import torch
     from unsloth import FastLanguageModel
     from datasets import load_dataset
@@ -147,3 +181,215 @@ def run(args):
                 model.push_to_hub_merged(args.save_path, tokenizer, args.hub_token)
     else:
         print("Warning: The model is not saved!")
+
+
+def parse(input_args=None):
+    # Define argument parser
+    parser = argparse.ArgumentParser(
+        description="🦥 Fine-tune your llm faster using unsloth!"
+    )
+
+    model_group = parser.add_argument_group("🤖 Model Options")
+    model_group.add_argument(
+        "--model_name",
+        type=str,
+        default="unsloth/llama-3-8b",
+        help="Model name to load",
+    )
+    model_group.add_argument(
+        "--max_seq_length",
+        type=int,
+        default=2048,
+        help="Maximum sequence length, default is 2048. We auto support RoPE Scaling internally!",
+    )
+    model_group.add_argument(
+        "--dtype",
+        type=str,
+        default=None,
+        help="Data type for model (None for auto detection)",
+    )
+    model_group.add_argument(
+        "--load_in_4bit",
+        action="store_true",
+        help="Use 4bit quantization to reduce memory usage",
+    )
+    model_group.add_argument(
+        "--dataset",
+        type=str,
+        default="yahma/alpaca-cleaned",
+        help="Huggingface dataset to use for training",
+    )
+
+    lora_group = parser.add_argument_group(
+        "🧠 LoRA Options", "These options are used to configure the LoRA model."
+    )
+    lora_group.add_argument(
+        "--r",
+        type=int,
+        default=16,
+        help="Rank for Lora model, default is 16.  (common values: 8, 16, 32, 64, 128)",
+    )
+    lora_group.add_argument(
+        "--lora_alpha",
+        type=int,
+        default=16,
+        help="LoRA alpha parameter, default is 16. (common values: 8, 16, 32, 64, 128)",
+    )
+    lora_group.add_argument(
+        "--lora_dropout",
+        type=float,
+        default=0,
+        help="LoRA dropout rate, default is 0.0 which is optimized.",
+    )
+    lora_group.add_argument(
+        "--bias", type=str, default="none", help="Bias setting for LoRA"
+    )
+    lora_group.add_argument(
+        "--use_gradient_checkpointing",
+        type=str,
+        default="unsloth",
+        help="Use gradient checkpointing",
+    )
+    lora_group.add_argument(
+        "--random_state",
+        type=int,
+        default=3407,
+        help="Random state for reproducibility, default is 3407.",
+    )
+    lora_group.add_argument(
+        "--use_rslora", action="store_true", help="Use rank stabilized LoRA"
+    )
+    lora_group.add_argument(
+        "--loftq_config", type=str, default=None, help="Configuration for LoftQ"
+    )
+
+    training_group = parser.add_argument_group("🎓 Training Options")
+    training_group.add_argument(
+        "--per_device_train_batch_size",
+        type=int,
+        default=2,
+        help="Batch size per device during training, default is 2.",
+    )
+    training_group.add_argument(
+        "--gradient_accumulation_steps",
+        type=int,
+        default=4,
+        help="Number of gradient accumulation steps, default is 4.",
+    )
+    training_group.add_argument(
+        "--warmup_steps",
+        type=int,
+        default=5,
+        help="Number of warmup steps, default is 5.",
+    )
+    training_group.add_argument(
+        "--max_steps", type=int, default=400, help="Maximum number of training steps."
+    )
+    training_group.add_argument(
+        "--learning_rate",
+        type=float,
+        default=2e-4,
+        help="Learning rate, default is 2e-4.",
+    )
+    training_group.add_argument(
+        "--optim", type=str, default="adamw_8bit", help="Optimizer type."
+    )
+    training_group.add_argument(
+        "--weight_decay",
+        type=float,
+        default=0.01,
+        help="Weight decay, default is 0.01.",
+    )
+    training_group.add_argument(
+        "--lr_scheduler_type",
+        type=str,
+        default="linear",
+        help="Learning rate scheduler type, default is 'linear'.",
+    )
+    training_group.add_argument(
+        "--seed",
+        type=int,
+        default=3407,
+        help="Seed for reproducibility, default is 3407.",
+    )
+
+    # Report/Logging arguments
+    report_group = parser.add_argument_group("📊 Report Options")
+    report_group.add_argument(
+        "--report_to",
+        type=str,
+        default="tensorboard",
+        choices=[
+            "azure_ml",
+            "clearml",
+            "codecarbon",
+            "comet_ml",
+            "dagshub",
+            "dvclive",
+            "flyte",
+            "mlflow",
+            "neptune",
+            "tensorboard",
+            "wandb",
+            "all",
+            "none",
+        ],
+        help="The list of integrations to report the results and logs to. Supported platforms are: \n\t\t 'azure_ml', 'clearml', 'codecarbon', 'comet_ml', 'dagshub', 'dvclive', 'flyte', 'mlflow', 'neptune', 'tensorboard', and 'wandb'. Use 'all' to report to all integrations installed, 'none' for no integrations.",
+    )
+    report_group.add_argument(
+        "--logging_steps", type=int, default=1, help="Logging steps, default is 1"
+    )
+
+    # Saving and pushing arguments
+    save_group = parser.add_argument_group("💾 Save Model Options")
+    save_group.add_argument(
+        "--output_dir", type=str, default="outputs", help="Output directory"
+    )
+    save_group.add_argument(
+        "--save_model", action="store_true", help="Save the model after training"
+    )
+    save_group.add_argument(
+        "--save_method",
+        type=str,
+        default="merged_16bit",
+        choices=["merged_16bit", "merged_4bit", "lora"],
+        help="Save method for the model, default is 'merged_16bit'",
+    )
+    save_group.add_argument(
+        "--save_gguf",
+        action="store_true",
+        help="Convert the model to GGUF after training",
+    )
+    save_group.add_argument(
+        "--save_path", type=str, default="model", help="Path to save the model"
+    )
+    save_group.add_argument(
+        "--quantization",
+        type=str,
+        default="q8_0",
+        nargs="+",
+        help="Quantization method for saving the model. common values ('f16', 'q4_k_m', 'q8_0'), Check our wiki for all quantization methods https://github.com/unslothai/unsloth/wiki#saving-to-gguf ",
+    )
+
+    push_group = parser.add_argument_group("🚀 Push Model Options")
+    push_group.add_argument(
+        "--push_model",
+        action="store_true",
+        help="Push the model to Hugging Face hub after training",
+    )
+    push_group.add_argument(
+        "--push_gguf",
+        action="store_true",
+        help="Push the model as GGUF to Hugging Face hub after training",
+    )
+    push_group.add_argument(
+        "--hub_path",
+        type=str,
+        default="hf/model",
+        help="Path on Hugging Face hub to push the model",
+    )
+    push_group.add_argument(
+        "--hub_token", type=str, help="Token for pushing the model to Hugging Face hub"
+    )
+
+    return parser.parse_args([], namespace=argparse.Namespace(**input_args))
